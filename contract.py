e69b40f1e764049ee6ffee899a95854bd2e042f7
Jordi Esteve <jesteve@zikzakmedia.com>
2016-06-21 19:35:13 +0200

    Add merged invoice line in consumption form view

--- contract.py
+++ contract.py
@@ -2,8 +2,8 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import PoolMeta
-
 from trytond.model import fields
+from trytond.pyson import Eval
 
 
 __all__ = ['ContractConsumption']
@@ -13,7 +13,10 @@ __metaclass__ = PoolMeta
 class ContractConsumption:
     __name__ = 'contract.consumption'
     invoice_line = fields.Many2One('account.invoice.line',
-        'Invoice Line', readonly=True)
+        'Merged Invoice Line', readonly=True,
+        states={
+            'invisible': ~Eval('invoice_line'),
+        })
 
     @classmethod
     def _invoice(cls, consumptions):
